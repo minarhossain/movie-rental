1498e2ea4d3f4ee06ccb6b0ab5457e50e43f0268
minarhossain <codewithmnr@gmail.com>
2024-05-21 23:30:58 +0600

    tailwind css and image file complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
-    <>
-      <h1>Movie Rental</h1>
-    </>
+    <div>
+      <Header />
+    </div>
   );
 }
 
 export default App;
+
+// createContext(defaultValue) return object
+// SomeContext.Provider (Wrap components with a provider)
+// SomeContext.Consumer (Consume the context)
+
+// Header (logo, icons (add to card numbers), etc)
+
+// sidebar movie list (movie card, => image, rating, onClick(add to card), onClick (details) ,  )
+
+// app => header , main (app, sidebar, movie list) footer
